a1ed4af979239e34d98a725c69afa7ca554e9ca4
bm4656 <newjin46@gmail.com>
2025-03-07 00:22:12 +0900

    [미션1] 깃허브 리포지토리에 프로젝트 올리기

---
+++ src/main/kotlin/com/example/kopring_playlist/KopringPlaylistApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kopring_playlist
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KopringPlaylistApplication
+
+fun main(args: Array<String>) {
+	runApplication<KopringPlaylistApplication>(*args)
+}
